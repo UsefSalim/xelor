d217303e42bb4a998bfc32e624112acb5ea1669b
UsefSalim <salimguithub@gmail.com>
2021-03-17 15:29:22 -0300

    fix eslint beug

--- utilities/generator/eslintPrettier.js
+++ utilities/generator/eslintPrettier.js
@@ -7,16 +7,16 @@ exports.configEslintPrettier = () => {
   if (!fs.existsSync('./vscode')) {
     fs.mkdir(`${path}/.vscode`, () => {
       fs.appendFile(
-        `${path}/.vscode/go.settings.json`,
+        `${path}/.vscode/settings.json`,
         `{
-"editor.formatOnSave": true,
-"editor.tabSize": 2,
-"[javascript]": {
-"editor.formatOnSave": false
-},
-"editor.codeActionsOnSave": {
-  "source.fixAll": true
-}
+  "editor.formatOnSave": true,
+  "editor.tabSize": 2,
+  "[javascript]": {
+  "editor.formatOnSave": false
+  },
+  "editor.codeActionsOnSave": {
+    "source.fixAll": true
+  }
 }`,
         (err) => {
           if (err) throw err;
@@ -25,16 +25,16 @@ exports.configEslintPrettier = () => {
     });
   } else {
     fs.appendFile(
-      `${path}/.vscode/go.settings.json`,
+      `${path}/.vscode/settings.json`,
       `{
-"editor.formatOnSave": true,
-"editor.tabSize": 2,
-"[javascript]": {
-  "editor.formatOnSave": false
-},
-"editor.codeActionsOnSave": {
-  "source.fixAll": true
-}
+  "editor.formatOnSave": true,
+  "editor.tabSize": 2,
+  "[javascript]": {
+    "editor.formatOnSave": false
+  },
+  "editor.codeActionsOnSave": {
+    "source.fixAll": true
+  }
 }`,
       (err) => {
         if (err) throw err;
@@ -44,13 +44,13 @@ exports.configEslintPrettier = () => {
   fs.appendFile(
     `${path}/.prettierrc`,
     `{
-"printWidth": 80,
-"tabWidth": 2,
-"semi": true,
-"singleQuote": true,
-"trailingComma": "es5",
-"bracketSpacing": true,
-"jsxBracketSameLine": false
+  "printWidth": 80,
+  "tabWidth": 2,
+  "semi": true,
+  "singleQuote": true,
+  "trailingComma": "es5",
+  "bracketSpacing": true,
+  "jsxBracketSameLine": false
 }`,
     (err) => {
       if (err) throw err;
@@ -59,47 +59,47 @@ exports.configEslintPrettier = () => {
   fs.appendFile(
     `${path}/.eslintrc.json`,
     `{
-"env": {
-  "browser": true,
-  "commonjs": true,
-  "es2021": true
-},
-"extends": ["airbnb-base", "prettier"],
-"plugins": ["prettier"],
-"parserOptions": {
-  "ecmaVersion": 12
-},
-"rules": {
-  "prettier/prettier": [
-    "error",
-    {
-      "endOfLine": "auto"
-    }
-  ],
-  "import/no-extraneous-dependencies": "off",
-  "import/prefer-default-export": "off",
-  "no-confusing-arrow": "off",
-  "linebreak-style": "off",
-  "no-unused-expressions": "off",
-  "no-use-before-define": "off",
-  "no-shadow": "off",
-  "arrow-parens": ["off"],
-  "comma-dangle": [
-    "error",
-    {
-      "arrays": "always-multiline",
-      "objects": "always-multiline",
-      "imports": "always-multiline",
-      "exports": "always-multiline",
-      "functions": "ignore"
-    }
-  ],
-  "no-plusplus": "off",
-  "complexity": ["error", 4],
-  "default-case": "error",
-  "import/no-unresolved" :"off"
-}
-    }`,
+  "env": {
+    "browser": true,
+    "commonjs": true,
+    "es2021": true
+  },
+  "extends": ["airbnb-base", "prettier"],
+  "plugins": ["prettier"],
+  "parserOptions": {
+    "ecmaVersion": 12
+  },
+  "rules": {
+    "prettier/prettier": [
+      "error",
+      {
+        "endOfLine": "auto"
+      }
+    ],
+    "import/no-extraneous-dependencies": "off",
+    "import/prefer-default-export": "off",
+    "no-confusing-arrow": "off",
+    "linebreak-style": "off",
+    "no-unused-expressions": "off",
+    "no-use-before-define": "off",
+    "no-shadow": "off",
+    "arrow-parens": ["off"],
+    "comma-dangle": [
+      "error",
+      {
+        "arrays": "always-multiline",
+        "objects": "always-multiline",
+        "imports": "always-multiline",
+        "exports": "always-multiline",
+        "functions": "ignore"
+      }
+    ],
+    "no-plusplus": "off",
+    "complexity": ["error", 6],
+    "default-case": "error",
+    "import/no-unresolved" :"off"
+  }
+}`,
     (err) => {
       if (err) throw err;
     }
