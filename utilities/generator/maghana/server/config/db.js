4beb79bcc13d8250ba8906956c0de1185bed14e1
xelorNpm <salimguithub@gmail.com>
2021-04-29 01:11:41 +0000

    fix beug creation file into generat folder

--- utilities/generator/maghana/server/config/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log('Mongo Db Connected'))
-  .catch((err) => console.log("error connection to the DataBase :"  + err));
